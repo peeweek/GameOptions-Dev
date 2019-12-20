1cafd9a443ae33ee1e867d49902623c0fb250ada
Thomas ICHÉ <peeweek@gmail.com>
2019-12-20 09:18:25 +0100

    Base Files

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 RotationAxis = Vector3.up;
+    public float RotationRate = 15.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotationAxis, RotationRate * Time.deltaTime);
+    }
+}
