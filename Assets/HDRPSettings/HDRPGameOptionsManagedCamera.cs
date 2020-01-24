1d7266943a7cea134caf0fe24f34164704b2e21b
Thomas ICHÉ <peeweek@gmail.com>
2020-01-24 18:06:05 +0100

    Added HDRP Options (Base, only Anti Aliasing)

---
+++ Assets/HDRPSettings/HDRPGameOptionsManagedCamera.cs
@@ -0,0 +1,19 @@
+﻿using GameOptionsUtility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+[RequireComponent(typeof(HDAdditionalCameraData))]
+public class HDRPGameOptionsManagedCamera : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        HDRPCameraOption.AddCamera(GetComponent<HDAdditionalCameraData>());
+    }
+
+    private void OnDisable()
+    {
+        HDRPCameraOption.RemoveCamera(GetComponent<HDAdditionalCameraData>());
+    }
+}
